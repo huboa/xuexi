a885671565c3f9dca32683eed86ec337a421746c
work <wxcr11@gmail.com>
2016-11-16 19:05:06 +0800

    作业ok

--- homework/day06/计算器.py
+++ homework/day06/计算器.py
@@ -11,8 +11,13 @@ def compute(m,x,y):
     elif m == "*":
        return x * y
     elif m == "/" :
-       return x / y
+        return round(x/y, 2)
+
+
     else:
         print("运算符号错误")
 
-print(compute("+",int(2),int(1)))
+a=compute("/",float(10),float(3))
+
+print("a",a)
+

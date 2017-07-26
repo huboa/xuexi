ad7d4182b4201b261da471c51130d1f6463d7d0f
work <wxcr11@gmail.com>
2017-07-26 19:45:28 +0800

    remove dir

--- linhaifeng/day04-函数嵌套/eval-用法.py
+++ linhaifeng/day04-函数嵌套/eval-用法.py
@@ -5,16 +5,34 @@ import os
 # eval(cmd)
 #
 dic='{"name":"alex","password":"alex3714"}'
-# print(eval(dic),type(eval(dic)))
+print(eval(dic),type(eval(dic)))
+dic1=eval(dic)
 # d=eval(dic)
+# print(dic1["name"])
+# for n in dic1:
+#      print(n,dic1[n])
+# print(dic)
 
 with open('db.txt','w',encoding='utf-8') as f:
-     f.write(dic)
-     f.write(dic)
+    f.write(dic+"\n")
+    f.writelines(dic+"\n")
+with open('db.txt','r',encoding='utf-8') as f:
+    print(f.read())
 with open('db.txt', 'r', encoding='utf-8') as f:
-     lines=f.readline()
+    for n in f:
+        f.readline()
 
-     print(f.read())
-     dic=f.read()
-print(lines)
-print(type((lines)))
+#
+# with open('db.txt','w',encoding='utf-8') as f:
+#       for i in dic:
+#       #    f.writelines(i)
+#           print(i)
+#      f.writelines(dic)
+# with open('db.txt', 'r', encoding='utf-8') as f:
+#      for line in f:
+#           print(line)
+
+# line     print(f.read())
+#      dic=f.read()
+# print(lines)
+# print(type((lines)))

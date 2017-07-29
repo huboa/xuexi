8984f2e8c3a75c3993ea237434236b5e6ade0d04
huboa <wxcr11@gmail.com>
2017-07-29 11:40:01 +0800

    mac new

--- linhaifeng/day05-函数-模块与包的使用/02-面向过程编程.py
+++ linhaifeng/day05-函数-模块与包的使用/02-面向过程编程.py
@@ -5,3 +5,26 @@
 #应用场景：
 
 ##grep -rl 'error' /dir/
+
+
+##第一阶段：找到文件的绝对路径
+
+##第二阶段 ：打开文件
+
+##第三阶段：循环读出每一行内容
+
+##第四阶段： 过滤
+
+##第五阶段： 打印文件名
+
+
+import os
+def search(filepath):
+    l=[]
+    g=os.walk(filepath)
+
+    for pardir,-,files in g:
+        for file in files:
+            abspath=r'%s\%s' %(pardir,file)
+            print(abspath)
+

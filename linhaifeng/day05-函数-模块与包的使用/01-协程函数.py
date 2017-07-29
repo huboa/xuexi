fb673cb5d4e72defbe74b8b12119737a828f4ea3
huboa <wxcr11@gmail.com>
2017-07-29 10:55:02 +0800

    mac new

--- linhaifeng/day05-函数-模块与包的使用/01-协程函数.py
+++ linhaifeng/day05-函数-模块与包的使用/01-协程函数.py
@@ -81,7 +81,14 @@
 
 
 ###解决初始化问题加装饰器
+def init(func):
+    def wrapper(*args,**kwargs):
+        g=func(*args,**kwargs)
+        next(g)
+        return g
+    return wrapper
 
+@init
 def eater(name):
     print('%s start to eat' %name)
     food_list=[]
@@ -94,15 +101,6 @@ def eater(name):
 def producer():
 
     zsc_g=eater('zsc')
-    # print(next(zsc_g))
-    # print(next(zsc_g))
-
-    ##第一阶段：初始化
-    next(zsc_g)##等于 zsc_g.send(None)
-    #zsc_g.send(None)
-
-
-    ##第二阶段：给yield 传值
     while True:
         food=input(">>: ").strip()
         if not food:continue

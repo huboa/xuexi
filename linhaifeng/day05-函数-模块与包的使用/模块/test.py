2e3ececec9eac01f8e9218773cc8dcafb4295104
huboa <wxcr11@gmail.com>
2017-07-29 15:25:02 +0800

    mac new

--- linhaifeng/day05-函数-模块与包的使用/模块/test.py
+++ linhaifeng/day05-函数-模块与包的使用/模块/test.py
@@ -2,14 +2,17 @@
 #1执行源文件
 #2以一个源文件的全局名称空间
 #3在当前位置拿到一个模块名，指向2创建的名称空间
-import spam
-money = 10000000
-def read1():
-    print('from test')
-print(spam.money)
-print(spam.read1)
-spam.read1()
-spam.read2()
-spam.change()
-print(money)
-spam.read1()
+# import spam
+# money = 10000000
+# def read1():
+#     print('from test')
+# print(spam.money)
+# print(spam.read1)
+# spam.read1()
+# spam.read2()
+# spam.change()
+# print(money)
+# spam.read1()
+
+import spam as s1
+print(s1.money)

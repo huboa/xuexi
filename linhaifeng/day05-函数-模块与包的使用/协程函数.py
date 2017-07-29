e6ca7f34f0bee380a5901e15ccb81782628fff3a
huboa <wxcr11@gmail.com>
2017-07-29 10:00:02 +0800

    mac new

--- linhaifeng/day05-函数-模块与包的使用/协程函数.py
+++ linhaifeng/day05-函数-模块与包的使用/协程函数.py
@@ -13,6 +13,17 @@
 
 
 #yield 的表达式形式的应用
+# def eater(name):
+#     print('%s start to eat' %name)
+#     while True:
+#         food=yield
+#         print('%s eat %s' %(name,food))
+#
+# zsc_g=eater('zsc')
+# print(next(zsc_g))
+# print(next(zsc_g))
+
+##用法
 def eater(name):
     print('%s start to eat' %name)
     while True:
@@ -22,3 +33,8 @@ def eater(name):
 zsc_g=eater('zsc')
 print(next(zsc_g))
 print(next(zsc_g))
+
+##第一阶段：初始化
+next(zsc_g)
+##第二阶段：给yield 传值
+zsc_g.send('粥')

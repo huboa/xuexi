1207d94dc0c0fe2913049f5e9a40d8956e4e8cb0
huboa <wxcr11@gmail.com>
2017-07-29 15:20:01 +0800

    mac new

--- linhaifeng/day05-函数-模块与包的使用/模块/spam.py
+++ linhaifeng/day05-函数-模块与包的使用/模块/spam.py
@@ -2,8 +2,12 @@ print('from the spam.py')
 
 money=1000
 def read1():
-    print('spam >> read1 >>>')
+    print('spam >> read1 >> money',money)
+
 def read2():
-    print()
+    print('spam >> read2 calling read')
+    read1()
 
 def change():
+    global money
+    money=0
--- linhaifeng/day05-函数-模块与包的使用/模块/test.py
+++ linhaifeng/day05-函数-模块与包的使用/模块/test.py
@@ -1,3 +1,15 @@
 ##导入模块做了那些事
 #1执行源文件
-#
+#2以一个源文件的全局名称空间
+#3在当前位置拿到一个模块名，指向2创建的名称空间
+import spam
+money = 10000000
+def read1():
+    print('from test')
+print(spam.money)
+print(spam.read1)
+spam.read1()
+spam.read2()
+spam.change()
+print(money)
+spam.read1()

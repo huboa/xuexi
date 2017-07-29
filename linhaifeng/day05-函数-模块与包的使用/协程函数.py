a94bb6bc8845574c1a2ff1a40e93fd469bf77312
huboa <wxcr11@gmail.com>
2017-07-29 10:35:02 +0800

    mac new

--- linhaifeng/day05-函数-模块与包的使用/协程函数.py
+++ linhaifeng/day05-函数-模块与包的使用/协程函数.py
@@ -23,24 +23,58 @@
 # print(next(zsc_g))
 # print(next(zsc_g))
 
+
+#########
+# ##用法
+# def eater(name):
+#     print('%s start to eat' %name)
+#     food_list=[]
+#     while True:
+#         food=yield  food_list
+#         food_list.append(food)  ##[保存列表]
+#         print('%s eat %s' %(name,food))
+#
+# zsc_g=eater('zsc')
+# # print(next(zsc_g))
+# # print(next(zsc_g))
+#
+# ##第一阶段：初始化
+# next(zsc_g)##等于 zsc_g.send(None)
+# #zsc_g.send(None)
+#
+#
+# ##第二阶段：给yield 传值
+# zsc_g.send('粥')
+# print(zsc_g.send('大虾'))
+
+
+######
 ##用法
 def eater(name):
     print('%s start to eat' %name)
     food_list=[]
     while True:
         food=yield  food_list
-        food_list.append(food)
+        food_list.append(food)  ##[保存列表]
         print('%s eat %s' %(name,food))
 
-zsc_g=eater('zsc')
-# print(next(zsc_g))
-# print(next(zsc_g))
 
-##第一阶段：初始化
-next(zsc_g)##等于 zsc_g.send(None)
-#zsc_g.send(None)
+def producer():
+
+    zsc_g=eater('zsc')
+    # print(next(zsc_g))
+    # print(next(zsc_g))
+
+    ##第一阶段：初始化
+    next(zsc_g)##等于 zsc_g.send(None)
+    #zsc_g.send(None)
 
 
-##第二阶段：给yield 传值
-zsc_g.send('粥')
-print(zsc_g.send('大虾'))
+    ##第二阶段：给yield 传值
+    while True:
+        food=input(">>: ").strip()
+        if not food:continue
+        print(zsc_g.send(food))
+
+
+producer()

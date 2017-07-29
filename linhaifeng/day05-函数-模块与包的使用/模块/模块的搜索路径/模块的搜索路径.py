02d79c080463210ac9d415e847cb9edb5e4e9aef
huboa <wxcr11@gmail.com>
2017-07-29 16:50:01 +0800

    mac new

--- linhaifeng/day05-函数-模块与包的使用/模块/模块的搜索路径/模块的搜索路径.py
+++ linhaifeng/day05-函数-模块与包的使用/模块/模块的搜索路径/模块的搜索路径.py
@@ -3,4 +3,6 @@
 
 import sys
 print(sys.path)
-/Users/ZSC/git/xuexi-mac/linhaifeng/day05-函数-模块与包的使用/模块/aaa
+print(type(sys.path))
+
+sys.path.append("/Users/ZSC/git/xuexi-mac/linhaifeng/day05-函数-模块与包的使用/模块/aaa")

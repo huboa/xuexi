8fda1d8a5079a4e3fd4570a0c91fa8d29bf1983d
huboa <wxcr11@gmail.com>
2017-07-30 11:00:02 +0800

    mac new

--- linhaifeng/day02-列表-字典/home-work/购物车.py
+++ linhaifeng/day02-列表-字典/home-work/购物车.py
@@ -1,10 +1,10 @@
 
-goods = [
+goods = {
     {"name": "电脑", "price": 1999},
     {"name": "鼠标", "price": 10},
     {"name": "游艇", "price": 20},
     {"name": "美女", "price": 998},
-]
+}
 
 for n in goods:
     print(n)

243df62f328f57ee614251fab9bdf51bbdd29d5f
huboa <wxcr11@gmail.com>
2017-07-30 15:35:01 +0800

    mac new

--- linhaifeng/day02-列表-字典/home-work/购物车.py
+++ linhaifeng/day02-列表-字典/home-work/购物车.py
@@ -1,15 +1,16 @@
 
-goods = {
+goods = [
     {"name": "电脑", "price": 1999},
     {"name": "鼠标", "price": 10},
     {"name": "游艇", "price": 20},
     {"name": "美女", "price": 998},
-}
+]
 
 ###总资产
 monney=0
 
 ###显示
-
+print('==============商品信息===============')
 for n in goods:
-    print(n)
+    print(n[name])
+print('==============商品信息===============')

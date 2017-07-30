890ed43921308e02d005d2e557d8a0ea23364b58
huboa <wxcr11@gmail.com>
2017-07-30 12:50:01 +0800

    mac new

--- linhaifeng/day02-列表-字典/home-work/菜单.py
+++ linhaifeng/day02-列表-字典/home-work/菜单.py
@@ -14,14 +14,14 @@ print(dic)
 
 level=dic
 
-
-while True:
-    for n in level:
-        print(n)
-    key = input(" 请输入 >> :").strip()
-
-    print('level====', type(level))
-    level=dic[key]
+print(level[河北])
+# while True:
+#     for n in level:
+#         print(n)
+#     key = input(" 请输入 >> :").strip()
+#
+#     print('level====', type(level))
+#     level=dic[key]
 
 
     #     for key2 in dic[key1]:

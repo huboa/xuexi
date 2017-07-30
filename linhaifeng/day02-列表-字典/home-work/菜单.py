9312d7372d9df2f665cdd592e207deb4d5ca927b
huboa <wxcr11@gmail.com>
2017-07-30 13:30:02 +0800

    mac new

--- linhaifeng/day02-列表-字典/home-work/菜单.py
+++ linhaifeng/day02-列表-字典/home-work/菜单.py
@@ -14,15 +14,17 @@ print(dic)
 
 level=dic
 
-
 while True:
     for n in level:
         print(n)
     key = input(" 请输入 >> :").strip()
 
     print('level====', type(level),len(level))
+    if key == "q" :
+        exit()
     if isinstance(level,dict):  ###如果是字典则进入不是则不进入
         level=level[key]
+
 
 
     #     for key2 in dic[key1]:

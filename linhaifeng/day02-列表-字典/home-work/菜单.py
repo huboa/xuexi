04ac9118a7846816feb8799f5f2cdd87b5d930b4
huboa <wxcr11@gmail.com>
2017-07-30 15:00:02 +0800

    mac new

--- linhaifeng/day02-列表-字典/home-work/菜单.py
+++ linhaifeng/day02-列表-字典/home-work/菜单.py
@@ -18,7 +18,8 @@ back_level=[]
 while True:
     for n in level:
         print(n)
-    key = input("请输入: e退出 m主菜单  -->> :").strip()
+
+    key = input("请输入: q退出 m主菜单  -->> :").strip()
 
     if key == ""  :continue
     if key == "q" :exit()   ##退出
@@ -26,12 +27,14 @@ while True:
     if key == "m" :   ###回退主菜单
         level=dic
         continue
+        
+    if key in level:  ##判断在不在里面
+        level = level[key]
 
-    if isinstance(level,dict):  ###如果是字典则进入不是则不进入
-        print("已经是最小菜单")
+    if isinstance(level,list):  ###如果是字典则进入不是则不进入
+        print("已经是最后一层")
         continue
 
-    if key in level:        ##判断在不在里面
-            level=level[key]
+
     else:
         print("请输入正确选项")

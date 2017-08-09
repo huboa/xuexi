81f328e56c6a24e805f33d2d69f236a1e3b69ef4
huboa <wxcr11@gmail.com>
2017-08-09 23:25:02 +0800

    mac new

--- linhaifeng/day06-模块-面向对象/homework/计算器.py
+++ linhaifeng/day06-模块-面向对象/homework/计算器.py
@@ -14,6 +14,7 @@ expression='1-2*((60+2*(-3-40.0/5)*(9-2*5/3+7/3*99/4*2998+10*568/14))-(-4*3)/(16
 #主函数
 def main1():
     content = re.search('\(([\-\+\*\/]*\d+\.?\d*)+\)', expression).group()
+#    content = re.search('\(([\-\+\*\/]*\d+\.?\d*)+\)', expression).group()
     print(content)
 main1()
 

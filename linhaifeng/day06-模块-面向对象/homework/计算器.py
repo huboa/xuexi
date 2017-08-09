d160de27e0ecf505098d60002ca1ba2bd1aa99ca
huboa <wxcr11@gmail.com>
2017-08-09 20:45:01 +0800

    mac new

--- linhaifeng/day06-模块-面向对象/homework/计算器.py
+++ linhaifeng/day06-模块-面向对象/homework/计算器.py
@@ -1,3 +1,4 @@
+import re
 
 #加减函数
 def plus_add():
@@ -12,6 +13,8 @@ def times_divide():
 expression='1-2*((60+2*(-3-40.0/5)*(9-2*5/3+7/3*99/4*2998+10*568/14))-(-4*3)/(16-3*2))'
 #主函数
 def main1():
-    pass
+    content = re.search('\(([\-\+\*\/]*\d+\.?\d*)+\)', expression).group()
+    print(content)
+main1()
 
 

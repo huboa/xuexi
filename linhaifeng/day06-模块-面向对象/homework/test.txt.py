9297110da8e12dfb7f06ab9c2b875bf81e5aba85
huboa <wxcr11@gmail.com>
2017-08-10 21:35:01 +0800

    mac new

--- linhaifeng/day06-模块-面向对象/homework/test.txt.py
+++ linhaifeng/day06-模块-面向对象/homework/test.txt.py
@@ -1,2 +1,19 @@
 import re
-print(re.sub('([\d+\.?\d*])\*([\d+\.?\d*])',r'\2\1','60+2*-3'))
+res='60+2*-3'
+def multiply(res):
+    list = re.findall('[\/\-]?\d+\.?\d*', res)
+    list1 = re.findall('\/\d+\.?\d*', res)
+    print(list,'list')
+    print(list1, 'list1')
+    sum1 = sum2 = 1
+    for n in list1:
+        list.remove(n)
+        n = re.search('\d+\.?\d*', n).group()
+        sum1 = sum1 * float(n)
+    for n in list:
+        n = re.search('\d+\.?\d*', n).group()
+        sum2 = sum2 * float(n)
+    # print(list)
+    return str(sum2/sum1)
+
+multiply(res)

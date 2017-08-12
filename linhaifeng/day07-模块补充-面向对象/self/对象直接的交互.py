4ba675b9fce3491dcfd338c72762ac97fdce6bed
huboa <wxcr11@gmail.com>
2017-08-12 15:20:01 +0800

    mac new

--- linhaifeng/day07-模块补充-面向对象/self/对象直接的交互.py
+++ linhaifeng/day07-模块补充-面向对象/self/对象直接的交互.py
@@ -1,5 +1,22 @@
 class Garen:
     camp='Demacia'
-    def __init__(self,nickname,lif_value=,aggresivity):
+    def __init__(self,nickname,life_value=100,aggresivity=80):
+        self.nickname=nickname
+        self.life_value=life_value
+        self.aggresivity=aggresivity
     def attack(self,enemy):
-        pass
+        enemy.life_value=self.aggresivity
+class Riven:
+    def __init__(self, nickname, life_value=100, aggresivity=80):
+        self.nickname = nickname
+        self.life_value = life_value
+        self.aggresivity = aggresivity
+
+    def attack(self, enemy):
+        enemy.life_value = self.aggresivity
+
+g1=Garen('boy')
+r1=Riven('girl')
+print(r1.life_value)
+g1.attack(r1)
+print(r1.life_value)

b2d0f5fc66d8edb55472ef280d9ec6191bc5359b
huboa <wxcr11@gmail.com>
2017-08-12 11:15:01 +0800

    mac new

--- linhaifeng/day07-模块补充-面向对象/self/04-subprocess/01-subprocess.py
+++ linhaifeng/day07-模块补充-面向对象/self/04-subprocess/01-subprocess.py
@@ -2,3 +2,9 @@ import subprocess
 res=subprocess.Popen('cat /Users/ZSC/scripts/remote.sh',
                      shell=True,stdout=subprocess.PIPE)
 print(res.stdout.read().decode('utf-8'))
+res=subprocess.Popen('cat1 /Users/ZSC/scripts/remote.sh',
+                     shell=True,
+                     stdout=subprocess.PIPE,
+                     sterr=subprocess.PIPE)
+#print(res.stdout.read().decode('utf-8'))
+print(res.stderr.read().decode('utf-8'))

916b8c91aa6f9a99355c7b9139cd9ecd12658508
huboa <wxcr11@gmail.com>
2017-08-12 15:35:00 +0800

    mac new

---
+++ linhaifeng/day07-模块补充-面向对象/self/05-继承/01-继承.py
@@ -0,0 +1,10 @@
+###继承的基本形式
+class ParentClass1:##定义父类
+    pass
+class ParentClass2:##定义父类
+    pass
+class SubClass1(ParentClass1):
+    pass
+class SubClass2(ParentClass1,ParentClass2):
+    pass
+print(SubClass1.__base__)

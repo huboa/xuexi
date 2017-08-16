7e7992f0103acbeae3598da6d33865de58d5a454
huboa <wxcr11@gmail.com>
2017-08-16 23:05:03 +0800

    mac new

--- linhaifeng/day07-模块补充-面向对象/homework/选课系统/core/auth.py
+++ linhaifeng/day07-模块补充-面向对象/homework/选课系统/core/auth.py
@@ -1,12 +1,15 @@
 import pickle,json,os,sys
+user_static={'user':None}
 def auth(func):
     def wrapper(*args,**kwargs):
+        if user_static['user']=
         name=input('name:').strip()
         password=input('password:').strip()
         dic = json.load(open('../db/user.json', 'r'))
         if name in dic and password == dic[name]["password"]:
 
             res=func(*args,**kwargs)
+
             return res
         else:
             print('user or passwd error')

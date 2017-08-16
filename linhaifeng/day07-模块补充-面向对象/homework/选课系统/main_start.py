7b7c3d0ec52fccf56f5c8467e28f23c956ff931a
work <wxcr11@gmail.com>
2017-08-16 09:30:00 +0800

    'windown10'

--- linhaifeng/day07-模块补充-面向对象/homework/选课系统/main_start.py
+++ linhaifeng/day07-模块补充-面向对象/homework/选课系统/main_start.py
@@ -1,20 +1,18 @@
 class school:
-    pass
+    def __init__(self, name, address,city):
+        self.name = name
+        self.address = address
+        self.city=city
 class student:
     def __init__(self, name, age, sex='male'):
         self.name = name
         self.age = age
         self.sex = sex
-
-    def learn(self):
-        print('%s is learning' % self.name)
-
-    def eat(self, y):
-        print('is eating')
-
-
 class teacher:
-    pass
+    def __init__(self, name, age, sex='male'):
+        self.name = name
+        self.age = age
+        self.sex = sex
 class course:
     def __init__(self,name,period,price):
         self.name=name
@@ -22,15 +20,29 @@ class course:
         self.price=price
     def tell_info(self):
         print('<课程名:%s 周期：%s 价格：%s>' %(self.name,self.period,self.price))
-
 class classes:
-    pass
+    def __init__(self,name,semester,course,date,teacher):
+        self.name=name
+        self.semester=semester
+        self.course=course
+        self.date=date
+        self.teacher=teacher
 class classes_record:
-    pass
+    def __init__(self,classes,times,date):
+        self.classes=classes
+        self.times=times
+        self.date=date
+
 class study_record:
-    pass
+   def __init__(self,classes_record,status,date,score):
+       self.classes_record=classes_record
+       self.status=status
+       self.date=date
+       self.score=score
 
 
+python=course('python','9','20000')
+python.tell_info()
 
 
 

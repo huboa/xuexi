5e2973a5b8e15da860d004658a1eba268caa3b8c
huboa <wxcr11@gmail.com>
2017-08-19 16:32:21 +0800

    mac new

--- linhaifeng/day08-接口-网络/self/01-网络编程/服务端.py
+++ linhaifeng/day08-接口-网络/self/01-网络编程/服务端.py
@@ -1,10 +1,19 @@
 ##买手机
+import socket
+phone=socket.socket(socket.AF_INET,socket.SOCK_STREAM)###tcp
+##udp
+
+
 
 #插卡
-
+phone.bind('127.0.0.1',8080)
 #开机
-
+phone.listen(5)
 
 #等电话
 
+###基于建立的连接，收发消息
 
+##挂电话
+
+###关机

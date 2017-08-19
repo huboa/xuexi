11de91bad48b3df1d5eb28eb17b6df350d36a83e
huboa <wxcr11@gmail.com>
2017-08-19 14:35:01 +0800

    mac new

--- linhaifeng/day08-接口-网络/self/07-item系列.py
+++ linhaifeng/day08-接口-网络/self/07-item系列.py
@@ -1,3 +1,16 @@
 class Foo:
     def __getitem__(self, item):
-        print()
+        print('===》get')
+        return self.__dict__[item]
+    def __setitem__(self, key, value):
+        self.__dict__[key]=value
+        #setattr(self,key,value)
+    def __delitem__(self, key):
+        self.__dict__.pop(key)
+f=Foo()
+#f.x=1
+print(f.x)
+print(f.__dict__)
+f['x']=1212121212
+del f['x']
+print(f['x'])

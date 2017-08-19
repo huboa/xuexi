1004ba9754b18fe3ba97f70d807cfab9fd20b323
huboa <wxcr11@gmail.com>
2017-08-19 17:05:01 +0800

    mac new

--- linhaifeng/day08-接口-网络/self/01-网络编程/服务端.py
+++ linhaifeng/day08-接口-网络/self/01-网络编程/服务端.py
@@ -1,28 +1,32 @@
 ##买手机
-import socket
-phone=socket.socket(socket.AF_INET,socket.SOCK_STREAM)###tcp
+import socket,time
+def net_conn():
+    phone=socket.socket(socket.AF_INET,socket.SOCK_STREAM)###tcp
 
-#插卡
-phone.bind(('127.0.0.1',8080))
-#开机
-phone.listen(5)
+    #插卡
+    phone.bind(('127.0.0.1',8080))
+    #开机
+    phone.listen(5)
 
-#等电话
-print('等待电话')
-conn,client_addr=phone.accept()
-print('连接')
-print(conn,client_addr)
-
-
-###基于建立的连接，收发消息
-client_data=conn.recv(1024)
-print('客户信息')
-print(client_data.upper())
-conn.send(client_data.upper())
+    #等电话
+    print('等待电话')
+    conn,client_addr=phone.accept()
+    print('连接')
+    print(conn,client_addr)
 
 
+    ###基于建立的连接，收发消息
+    client_data=conn.recv(1024)
+    print('客户信息')
+    print(client_data.upper())
+    conn.send(client_data.upper())
 
-##挂电话
-conn.close()
-###关机
-phone.close()
+
+
+    ##挂电话
+    conn.close()
+    ###关机
+    phone.close()
+
+while True:
+    net_conn()

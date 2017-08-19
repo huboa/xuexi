ca105ec0f28f16845eb7543ba7bcb4b143b0590f
huboa <wxcr11@gmail.com>
2017-08-19 14:50:01 +0800

    mac new

--- linhaifeng/day08-接口-网络/self/09-析构方法.py
+++ linhaifeng/day08-接口-网络/self/09-析构方法.py
@@ -2,8 +2,10 @@ class Foo:
     def __init__(self,x):
         self.x=x
 
-    def __del__(self):
+    def __del__(self):    ###在对象资源被释放的时候触发
         print('del-------')
+        print(self)
 
 f=Foo(100)
-print('===========')
+del f
+print('===========》')

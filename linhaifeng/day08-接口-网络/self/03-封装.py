91f8fb9c3af718fef5a60ce639d12e6229cb4a7d
huboa <wxcr11@gmail.com>
2017-08-19 14:05:01 +0800

    mac new

--- linhaifeng/day08-接口-网络/self/03-封装.py
+++ linhaifeng/day08-接口-网络/self/03-封装.py
@@ -23,30 +23,44 @@ class People:
         self.__Name=name
         self.__Age=age
     def tell_info(self):
-        print('tt')
+        print('名字：%s 年龄 %s'%(self.__Name,self.__Age))
+    def set_info(self,name,age):
+        if not isinstance(name,str):
+            raise TypeError('%s must be str' %name)
+        if not isinstance(age,int):
+            raise TypeError('%s must be str' %age)
 
-#取款是功能,而这个功能有很多功能组成:插卡、密码认证、输入金额、打印账单、取钱
-#对使用者来说,只需要知道取款这个功能即可,其余功能我们都可以隐藏起来,很明显这么做
-#隔离了复杂度,同时也提升了安全性
+        self.__Name=name
+        self.__Age=age
 
-class ATM:
-    def __card(self):
-        print('插卡')
-    def __auth(self):
-        print('用户认证')
-    def __input(self):
-        print('输入取款金额')
-    def __print_bill(self):
-        print('打印账单')
-    def __take_money(self):
-        print('取款')
-
-    def withdraw(self):
-        self.__card()
-        self.__auth()
-        self.__input()
-        self.__print_bill()
-        self.__take_money()
-
-a=ATM()
-a.withdraw()
+p=People('egon',18)
+p.tell_info()
+p.set_info('zsc',"19")
+p.set_info('zsc',19)
+p.tell_info()
+#
+# #取款是功能,而这个功能有很多功能组成:插卡、密码认证、输入金额、打印账单、取钱
+# #对使用者来说,只需要知道取款这个功能即可,其余功能我们都可以隐藏起来,很明显这么做
+# #隔离了复杂度,同时也提升了安全性
+#
+# class ATM:
+#     def __card(self):
+#         print('插卡')
+#     def __auth(self):
+#         print('用户认证')
+#     def __input(self):
+#         print('输入取款金额')
+#     def __print_bill(self):
+#         print('打印账单')
+#     def __take_money(self):
+#         print('取款')
+#
+#     def withdraw(self):
+#         self.__card()
+#         self.__auth()
+#         self.__input()
+#         self.__print_bill()
+#         self.__take_money()
+#
+# a=ATM()
+# a.withdraw()
--- linhaifeng/day08-接口-网络/self/05-静态属性.py
+++ linhaifeng/day08-接口-网络/self/05-静态属性.py
@@ -14,22 +14,3 @@ print(p.bmi)
 
 ###可以增删改查
 
-
-###反射的应用
-
-class Ftpserver:
-    def __init__(self,host,port):
-        self.host=host
-        self.port=port
-    def run(self):
-        while True:
-            cmd=input('>>:').strip()
-            if not cmd:continue
-            if hasattr(self,cmd):
-                func=getattr(self,cmd)
-                func
-    def get(self):
-        print('get func')
-    def put(self):
-        print('put func')
-f=Ftpserver('10.199.3.193',21)

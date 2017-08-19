cf8c835087879696871f2f2aa16c0149968ee671
huboa <wxcr11@gmail.com>
2017-08-19 17:25:02 +0800

    mac new

---
+++ linhaifeng/day08-接口-网络/self/03-链接循环/服务端.py
@@ -0,0 +1,30 @@
+##买手机
+import socket,time
+
+phone=socket.socket(socket.AF_INET,socket.SOCK_STREAM)###tcp
+phone.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1) ###复用
+#插卡
+phone.bind(('127.0.0.1',8080))
+#开机
+phone.listen(5)
+
+#等电话
+print('等待电话')
+conn,client_addr=phone.accept()
+print('连接')
+print(conn,client_addr)
+
+
+###基于建立的连接，收发消息
+while True:##通话交流
+    client_data=conn.recv(1024)
+    print('客户信息')
+    print(client_data.upper())
+    conn.send(client_data.upper())
+
+
+
+##挂电话
+conn.close()
+###关机
+phone.close()

0b45291d5968ded61179b4b06b88214d1ba90f1a
huboa <wxcr11@gmail.com>
2017-08-19 11:20:01 +0800

    mac new

--- linhaifeng/day08-接口-网络/self/03-封装.py
+++ linhaifeng/day08-接口-网络/self/03-封装.py
@@ -1,1 +1,15 @@
 #这种隐藏需要的问题：
+class Foo:
+    __N=11111 #_Foo__N
+    def __init__(self,name):
+        self.__Name=name#self._Foo__Name=name
+    def __f1(self):#_Foo__f1
+        print('f1')
+    def f2(self):
+        self.__f1()##self.Foo__f1
+
+f=Foo('zsc')
+print(f.__N)
+f.__f1()
+f.__Name
+f.f2()

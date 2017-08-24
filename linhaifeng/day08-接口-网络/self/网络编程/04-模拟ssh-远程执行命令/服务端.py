a20e0916f918af8e024b130d428987e29367288a
huboa <wxcr11@gmail.com>
2017-08-24 21:35:02 +0800

    mac new

--- linhaifeng/day08-接口-网络/self/网络编程/04-模拟ssh-远程执行命令/服务端.py
+++ linhaifeng/day08-接口-网络/self/网络编程/04-模拟ssh-远程执行命令/服务端.py
@@ -1,27 +1,20 @@
-##买手机
+####建立连接
 import socket
 import subprocess
-
 phone=socket.socket(socket.AF_INET,socket.SOCK_STREAM)###tcp
 phone.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1) ###复用
-#插卡
 phone.bind(('127.0.0.1',8080))
-#开机
 phone.listen(5)
 
-#等电话
 while True:   ###连接循环
-    print('等待电话')
     conn,client_addr=phone.accept()
-    print('连接')
     print(conn,client_addr)
 
-
     ###基于建立的连接，收发消息
-    while True:##通话交流
+    while True:
         try:
             cmd=conn.recv(1024)
-            if not cmd:break###针对对linux
+            if not cmd:break  ###针对对linux
             print('客户信息')
             res=subprocess.Popen(cmd.decode('utf-8'),
                                  shell=True,

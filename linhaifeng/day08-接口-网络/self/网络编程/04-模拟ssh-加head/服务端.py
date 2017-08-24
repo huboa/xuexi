96bae590bbf7a84e4af7e2ee7e21b6a6ae199c59
huboa <wxcr11@gmail.com>
2017-08-24 21:45:02 +0800

    mac new

--- linhaifeng/day08-接口-网络/self/网络编程/04-模拟ssh-加head/服务端.py
+++ linhaifeng/day08-接口-网络/self/网络编程/04-模拟ssh-加head/服务端.py
@@ -5,7 +5,7 @@ phone=socket.socket(socket.AF_INET,socket.SOCK_STREAM)###tcp
 phone.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1) ###复用
 phone.bind(('127.0.0.1',8080))
 phone.listen(5)
-
+print('server start...')
 while True:   ###连接循环
     conn,client_addr=phone.accept()
     print(conn,client_addr)
@@ -15,15 +15,13 @@ while True:   ###连接循环
         try:
             cmd=conn.recv(1024)
             if not cmd:break  ###针对对linux异常断开就跳出
-            print('客户信息')
+            print('cmd',cmd)
             res=subprocess.Popen(cmd.decode('utf-8'),
                                  shell=True,
                                  stdout=subprocess.PIPE,
                                  stderr=subprocess.PIPE)
             stdout=res.stdout.read()
             stderr=res.stderr.read()
-
-
             conn.send(stdout+stderr)
         except Exception: ##针对windows异常跳出
             break

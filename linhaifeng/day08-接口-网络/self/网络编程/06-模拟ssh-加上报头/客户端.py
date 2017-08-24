b752fb0395def2546a11b49e2f04edff867b07a2
huboa <wxcr11@gmail.com>
2017-08-24 22:05:01 +0800

    mac new

---
+++ linhaifeng/day08-接口-网络/self/网络编程/06-模拟ssh-加上报头/客户端.py
@@ -0,0 +1,15 @@
+##导入socket
+import socket
+phone=socket.socket(socket.AF_INET,socket.SOCK_STREAM)###tcp
+
+phone.connect(('127.0.0.1',8080))
+
+while True:
+    cmd=input('>>:').strip()
+    if not cmd:continue
+    phone.send(cmd.encode('utf-8'))
+    cmd_res=phone.recv(1024)
+    print(cmd_res.decode('utf-8'))
+    # print(cmd_res.decode('gbk'))
+
+phone.close()

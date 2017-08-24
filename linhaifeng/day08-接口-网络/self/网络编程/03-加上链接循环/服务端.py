d9efdf99f39212176f5657f5bcfff192267077e7
huboa <wxcr11@gmail.com>
2017-08-24 21:20:01 +0800

    mac new

--- linhaifeng/day08-接口-网络/self/网络编程/03-加上链接循环/服务端.py
+++ linhaifeng/day08-接口-网络/self/网络编程/03-加上链接循环/服务端.py
@@ -3,21 +3,16 @@ phone=socket.socket(socket.AF_INET,socket.SOCK_STREAM)###tcp
 phone.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1) ###复用
 phone.bind(('127.0.0.1',8080))
 phone.listen(5)
-print('启动服务')
+print('服务ing')
 while True:   ###连接循环
     conn,client_addr=phone.accept()
-    ###基于建立的连接，收发消息
-    while True:##通话交流
+    while True:###基于建立的连接，收发消息
         try:
             client_data=conn.recv(1024)
-            if not client_data:break###针对对linux
-            print('客户信息')
-            print(client_data.upper())
+            if not client_data:break
+            print(client_addr)
             conn.send(client_data.upper())
-        except Exception: ##针对windows
+        except Exception:
             break
-
-    ##挂电话
     conn.close()
-###关机
 phone.close()

f721f35a1faa12a5cd74762703d15797bd77c18e
huboa <wxcr11@gmail.com>
2017-08-26 14:55:01 +0800

    mac new

--- linhaifeng/day09-udp-线程/self/04-process对象的其他方法和属性方法.py
+++ linhaifeng/day09-udp-线程/self/04-process对象的其他方法和属性方法.py
@@ -15,22 +15,12 @@ def work(name):
 
 if __name__ == '__main__':
     p1=Process(target=work,args=('egon',))
-    p2=Process(target=work, args=('zsc',))
-    p3 = Process(target=work, args=('zxx',))
+
     # p=Process(target=work,kwargs={'name':'zsc'})
 
-    # p1.start()
-    # p2.start()
-    # p3.start()
-    # p1.join() ##主进程等，等待p1运行结束
-    # p2.join()  ##主进程等，等待p2运行结束
-    # p3.join()
-###循环写法
-    p_l=[p1,p2,p3]
-    for p in p_l:
-        p.start()
+    p1.start()
+    p1.terminate()
+    print(p1.is_alive())
 
-    for p in p_l:
-        p.join()
 
     print('主')

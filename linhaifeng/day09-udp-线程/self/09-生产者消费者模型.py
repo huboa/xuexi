f2bb2f9909eb2b1850f2c67d28f811fb4e80b994
huboa <wxcr11@gmail.com>
2017-08-26 16:35:01 +0800

    mac new

--- linhaifeng/day09-udp-线程/self/09-生产者消费者模型.py
+++ linhaifeng/day09-udp-线程/self/09-生产者消费者模型.py
@@ -1,1 +1,16 @@
-def
+from multiprocessing import Process,Queue
+import time,os
+def producer(q):
+    for i in range(10):
+        time.sleep(1)
+        res='包子%s'%i
+        q.put('<%s>生产了<>%s'%(os.getpid(),res))
+
+def consumer(q):
+    while True:
+        res = q.get()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    q=Queue()
+    

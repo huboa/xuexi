a1f6ecf521793de95e6d4c3aaf61f669d20029ab
huboa <wxcr11@gmail.com>
2017-08-26 14:35:01 +0800

    mac new

---
+++ linhaifeng/day09-udp-线程/self/03-join方法.py
@@ -0,0 +1,23 @@
+from multiprocessing import Process
+import time
+
+class MyProcess(Process):
+
+    def __init__(self,name):
+        super().__init__()
+        self.name=name
+
+
+def work(name):
+    print('task %s is running' %name)
+    time.sleep(2)
+    print('task %s is done' % name)
+
+if __name__ == '__main__':
+    p=Process(target=work,args=('egon',))
+    # p=Process(target=work,kwargs={'name':'zsc'})
+    p.start()
+    p1=Process(target=work, args=('zsc',))
+    p1.start()
+    p1.join()
+    print('主')

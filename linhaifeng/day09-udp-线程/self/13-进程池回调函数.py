5c4bed2dd72b7aae6c03180884ef6a993a962fe7
huboa <wxcr11@gmail.com>
2017-08-26 18:26:06 +0800

    mac new

--- linhaifeng/day09-udp-线程/self/13-进程池回调函数.py
+++ linhaifeng/day09-udp-线程/self/13-进程池回调函数.py
@@ -1,3 +1,16 @@
 import requests ###pip3 install requests
-respone=requests.get('https://www.baidu.com')
+import os
+def get_page(url):
+    print('<%s> get:%s'%(os.getpid(),url))
+    respone = requests.get(url)
+    if respone.status_code ==200:
+        return {'url':url,'test':respone.text}
+
+
+if __name__ == '__main__':
+    p=Pool(4)
+urls=[]
+
+r
 print(respone.status_code)
+

01dfa5ea3e8359564db8ebbb3a982fb4f7fe9399
huboa <wxcr11@gmail.com>
2017-08-26 10:20:00 +0800

    mac new

--- linhaifeng/day09-udp-线程/self/udp-并发/udp-服务端.py
+++ linhaifeng/day09-udp-线程/self/udp-并发/udp-服务端.py
@@ -1,9 +1,14 @@
 from socket import *
+import socketserver
+
+class MyUDPhandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        print(self.request)
+
+
+if __name__ == '__main__':
+
 udp_server=socket(AF_INET,SOCK_DGRAM)
 
 udp_server.bind(('127.0.0.1',8080))
 
-while True:
-    data,client_addr=udp_server.recvfrom(1024)
-    print(data,client_addr)
-    udp_server.sendto(data.upper(),client_addr)

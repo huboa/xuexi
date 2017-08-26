234581525ad2806312cb242b7562bb73bf0b1060
huboa <wxcr11@gmail.com>
2017-08-26 15:25:00 +0800

    mac new

--- linhaifeng/day09-udp-线程/self/06-同步锁.py
+++ linhaifeng/day09-udp-线程/self/06-同步锁.py
@@ -1,14 +1,16 @@
-from multiprocessing import Process
-import time
-def work(name):
-    print('task %s is running' %name)
+from multiprocessing import Process,Lock
+import time,os
+def work(name,*args,**kwargs):
+    mutex.acquire()
+    print('%s%stask %s is running' %(os.getpid(),os.getppid(),name))
     time.sleep(2)
-    print('task %s is done' % name)
-
+    print('%s%stask %s is done' %(os.getpid(),os.getppid(),name))
+    mutex.release()
 if __name__ == '__main__':
-    p=Process(target=work,args=('egon',))
+    mutex=Lock()
+    p=Process(target=work,args=('egon',mutex))
     # p=Process(target=work,kwargs={'name':'zsc'})
     p.start()
-    p1=Process(target=work, args=('zsc',))
+    p1=Process(target=work, args=('zsc',mutex))
     p1.start()
     print('主')

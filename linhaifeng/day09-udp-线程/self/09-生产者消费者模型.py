c67d4233f04d737ddca0b3295ca3b9192ce68197
huboa <wxcr11@gmail.com>
2017-08-26 16:40:01 +0800

    mac new

--- linhaifeng/day09-udp-线程/self/09-生产者消费者模型.py
+++ linhaifeng/day09-udp-线程/self/09-生产者消费者模型.py
@@ -4,13 +4,18 @@ def producer(q):
     for i in range(10):
         time.sleep(1)
         res='包子%s'%i
-        q.put('<%s>生产了<>%s'%(os.getpid(),res))
-
+        q.put(res)
+        print('\33[45m<%s> 生产[%s]\033[20m' % (os.getpid(), res))
 def consumer(q):
     while True:
         res = q.get()
-        time.sleep(1)
+        time.sleep(1.5)
+        print('\033[34m<%s> 吃了[%s]\033[0m'%(os.getpid(),res))
 
 if __name__ == '__main__':
     q=Queue()
-    
+    p1=Process(target=producer,args=(q,))
+    c1=Process(target=consumer,args=(q,))
+    p1.start()
+    c1.start()
+    print('主')

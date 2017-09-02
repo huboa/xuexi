b98fe7d2f571f03601b9a51e4b9cfa53181c5d0d
huboa <wxcr11@gmail.com>
2017-09-02 11:20:01 +0800

    mac new

--- linhaifeng/day10-线程-协程-IO模型/self/06-守护线程.py
+++ linhaifeng/day10-线程-协程-IO模型/self/06-守护线程.py
@@ -1,5 +1,27 @@
 from multiprocessing  import Process
+from threading import Thread
 import time
+###守护进程
+# def task():
+#     print('123')
+#     time.sleep(2)
+#     print('123done')
+#
+# def task2():
+#     print('456')
+#     time.sleep(5)
+#     print('456done')
+#
+#
+# if __name__ == "__main__":
+#     p=Process(target=task)
+#     p2=Process(task2())
+#     p.daemon=True  ###守护进程开启就忽略
+#     p.start()
+#     p2.start()
+#     print('主')
+
+###守护线程
 def task():
     print('123')
     time.sleep(2)
@@ -12,9 +34,9 @@ def task2():
 
 
 if __name__ == "__main__":
-    p=Process(target=task)
-    p2=Process(task2())
-    p.daemon=True  ###守护进程开启就忽略
+    p=Thread(target=task)
+    p2=Thread(task2())
+    # p.daemon=True  ###守护线程
     p.start()
     p2.start()
     print('主')

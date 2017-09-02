7ee65fea26ad68e99d5c9cdd32561265ade87719
huboa <wxcr11@gmail.com>
2017-09-02 10:55:01 +0800

    mac new

--- linhaifeng/day10-线程-协程-IO模型/self/05-Thread对象其他相关属性和方法.py
+++ linhaifeng/day10-线程-协程-IO模型/self/05-Thread对象其他相关属性和方法.py
@@ -1,9 +1,10 @@
-from threading import Thread,active_count
+from threading import Thread,active_count,enumerate,current_thread
 # from multiprocessing import Process
 import time
 
 def task():
     print('is running')
+    time(2)
 if __name__ == '__main__':
     t=Thread(target=task,)
     # t=Process(target=task,)
@@ -12,3 +13,5 @@ if __name__ == '__main__':
     print(t.is_alive())
     print(t.getName())
     print('主')
+    print(active_count())
+    # print(enumerate())

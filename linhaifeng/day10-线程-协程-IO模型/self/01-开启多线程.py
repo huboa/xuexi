85414242a4a4bf8a3423ed6e118529c5ebcc3154
huboa <wxcr11@gmail.com>
2017-09-02 10:20:01 +0800

    mac new

--- linhaifeng/day10-线程-协程-IO模型/self/01-开启多线程.py
+++ linhaifeng/day10-线程-协程-IO模型/self/01-开启多线程.py
@@ -1,7 +1,15 @@
 from threading import Thread
+from multiprocessing import Process
+
 def task():
     print('is running')
 if __name__ == '__main__':
     t=Thread(target=task,)
+    t=Process(target=task,)
     t.start()
     print('主')
+
+class MyThread(Thread):
+    def run(self):
+        print('is running')
+

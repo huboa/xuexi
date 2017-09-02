9f8518e190fe248dafcd65284d217c35b3cb97d6
huboa <wxcr11@gmail.com>
2017-09-02 15:40:03 +0800

    mac new

--- linhaifeng/day10-线程-协程-IO模型/self/信号量.py
+++ linhaifeng/day10-线程-协程-IO模型/self/信号量.py
@@ -1,4 +1,12 @@
 from threading import Thread,current_thread,Semaphore
 import time,random
-sm=Semaphore
-def work()
+sm=Semaphore(5)
+def work():
+    sm.acquire()
+    print('%s 工作'%current_thread().getName())
+    time.sleep(random.randint(1,3))
+    sm.release()
+if __name__ == '__main__':
+    for i in range(20):
+        t=Thread(target=work())
+        t.start()

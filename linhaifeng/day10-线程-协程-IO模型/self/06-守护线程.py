90c1711943da521f6539c43d2930973acacfbfd6
huboa <wxcr11@gmail.com>
2017-09-02 11:15:01 +0800

    mac new

--- linhaifeng/day10-线程-协程-IO模型/self/06-守护线程.py
+++ linhaifeng/day10-线程-协程-IO模型/self/06-守护线程.py
@@ -5,9 +5,16 @@ def task():
     time.sleep(2)
     print('123done')
 
+def task2():
+    print('456')
+    time.sleep(5)
+    print('456done')
+
+
 if __name__ == "__main__":
     p=Process(target=task)
-    p.daemon=True
+    p2=Process(task2())
+    p.daemon=True  ###守护进程开启就忽略
     p.start()
+    p2.start()
     print('主')
-    

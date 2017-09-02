aa824c6bbbde36852a324a1d09624521cf75d6ee
huboa <wxcr11@gmail.com>
2017-09-02 10:45:01 +0800

    mac new

--- linhaifeng/day10-线程-协程-IO模型/self/04-多线程共享内存地址-练习.py
+++ linhaifeng/day10-线程-协程-IO模型/self/04-多线程共享内存地址-练习.py
@@ -8,16 +8,22 @@ def talk():
         msg_l.append(msg)
 def format():
     while True:
-        if not msg_l:
+        if  msg_l:
             data=msg_l.pop()
             format_l.append(data.upper())
 def save():
     while True:
-        if format_l:
+        if  format_l:
             data=format_l.pop()
             with open('db.txt','a') as f:
                 f.write('%s\n'%data)
 
 
 if __name__ == '__main__':
-    t1=Thread
+    t1=Thread(target=talk)
+    t2=Thread(target=format)
+    t3=Thread(target=save)
+
+    t1.start()
+    t2.start()
+    t3.start()

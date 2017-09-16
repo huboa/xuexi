0408bed643fd42af79f779c56bf643df8ab29080
huboa <wxcr11@gmail.com>
2017-09-16 10:15:00 +0800

    mac new

--- linhaifeng/day12-前端/serverJD.py
+++ linhaifeng/day12-前端/serverJD.py
@@ -1,11 +1,11 @@
 import socket
 sock=socket.socket()
-sock.bind("127.0.0.1",8800)
+sock.bind("127.0.0.1",8080)
 sock.listen(5)
 while True:
     print("wating...")
-    conn,addr=sock.accept()##默认阻塞
+    conn,addr=sock.accept() ##默认阻塞
     data=conn.recv(1024)
     print('data',data)
-    conn.send(b"HTTP/1.1 201 OK \r\n hello")
+    conn.send(b"HTTP/1.1 201 OK \r\n \r\n <h1>hello<h1\>")
     conn.close()

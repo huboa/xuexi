5fd0d0f2bb19b851e18fd57c68e5472b31530bbb
work <wxcr11@gmail.com>
2017-10-12 09:48:40 +0800

    'windown10'

--- linhaifeng/day03-函数-file/homework-db/haproxy-修改配置文件/python_sed.py
+++ linhaifeng/day03-函数-file/homework-db/haproxy-修改配置文件/python_sed.py
@@ -1,3 +1,5 @@
+
+
 ###查询
 def fetch(data):
     #backend www.oldboy.com
@@ -6,38 +8,68 @@ def fetch(data):
     with  open('haproxy.conf','r') as f:
         tag=False
         for line in f:
-            if line.strip() == backend_data:  ###取开始
+            if line.strip() == backend_data:  ###取开始跳过本次取后面内容
                 tag=True
                 continue
-            if tag and line.startswith('backend'):  ###取结尾
+            if tag and line.startswith('backend'):  ###遇到结尾跳出循环
                 break
-            if tag and line:   ###取列表
+            if tag and line:   ###池子加入列表
                 record_list.append(line.strip())
+
     for line in record_list:
          print(line)
     return record_list
-
+###添加
 def add(data):
     # data={'backend':'www.oldboy.com','record':{'server':'1.1.1.11','weight':22,'maxconn':33}}
+    # data={'backend':'www.huboa.com','record':{'server':'1.1.1.11','weight':22,'maxconn':33}}
 
     backend=data['backend']   ####获取域名
-    record_list=fetch(backend)     ####查询域名存在不存在
+    record_list=fetch(backend)     ####查询域名backend存在不存在
 
     ####需要录入的域名
     backend_data = "backend %s" % backend
-    ####需要录入的池子内容
+    ####backend的池子内容
     current_record='server %s %s weight %s maxconn %s' %(data['record']['server'], \
-                                                         data['record']['server'], \
-                                                         data['record']['weight'],\
-                                                         data['record']['maxconn'])
+                                                             data['record']['server'], \
+                                                             data['record']['weight'],\
+                                                             data['record']['maxconn'])
 
 
-    if not record_list:
-        print('ext===============')
+    if record_list:  ###修改已经存在的backend
+        print("record_list--修改已经存在的", record_list)
+        record_list.insert(0, backend_data)
+        if current_record not in record_list:
+            record_list.append(current_record)
+        print("record_list--修改后的", record_list)
+
+        with  open('haproxy.conf', 'r') as read_file, \
+                open('haproxy_new.conf', 'w') as write_file:
+            tag = False
+            tag_s = False
+            for r_line in read_file:
+                if r_line.strip() == backend_data:  #给backend 打标签
+                    tag = True
+                    tag_s = True
+                    continue
+                if tag and r_line.startswith('backend'):
+                    continue
+
+                if tag :  ###如过遇到backend 执行更新
+                    for new_line in record_list:
+                        if new_line.startswith('backend'):
+                            write_file.write('\n' + new_line + '\n')
+                        else:
+                            write_file.write('%s%s\n' % (' ' * 8, new_line))
+
+
+                else:###复制其它数据
+                    write_file.write(r_line)
+    else: ###新建backend
         record_list.append(backend_data)
         record_list.append(current_record)
         with  open('haproxy.conf', 'r') as read_file, \
-                open('haproxy_new.conf', 'w') as write_file:
+               open('haproxy_new.conf', 'w') as write_file:
             for r_line in read_file:
                 write_file.write(r_line)
             for new_line in record_list:
@@ -45,28 +77,9 @@ def add(data):
                     write_file.write('\n'+new_line+'\n')
                 else:
                     write_file.write('%s%s\n'%(' '*8,new_line))
-    else:
-        print(record_list)
-        record_list.insert(0,backend_data)
-        if current_record not in record_list:
-            record_list.append(current_record)
 
-        with  open('haproxy.conf', 'r') as read_file, \
-                open('haproxy_new.conf', 'w') as write_file:
-            tag=False
-            for r_line in read_file:
-                if r_line.strip() == backend_data:
-                    tag=True
-                    continue
-                if not tag:
-                    write_file.write(r_line)
-                else:
-                    for new_line in record_list:
-                        if new_line.startswith('backend'):
-                            write_file.write('\n' + new_line + '\n')
-                        else:
-                            write_file.write('%s%s\n' % (' ' * 8, new_line))
 
+##删除
 def remove(data):
     pass
     # backend = data['backend']  ####获取域名
@@ -89,7 +102,7 @@ def remove(data):
     #             open('haproxy_new.conf', 'w') as write_file:
     #         for r_line in read_file:
     #             write_file.write(r_line)
-
+##主函数
 if __name__ == '__main__':
     msg='''
     1：查询
---
+++ linhaifeng/day03-函数-file/homework-db/haproxy-修改配置文件/test.py
@@ -0,0 +1,12 @@
+
+# tag = False
+# for n in range(100):
+#     if n == 10:
+#         tag = True
+#         continue
+#
+#     if not tag:
+#         print("nnnn not tag",n, not tag)
+#     else:
+#         print(n)
+print(bool([1]))

b53ae43423ba5f7ed79add5126b49c1075f6312d
huboa <wxcr11@gmail.com>
2017-12-16 17:25:01 +0800

    mac new

---
+++ linhaifeng/day23/评论树.py
@@ -0,0 +1,17 @@
+commentList=[
+    {"nid":1,"content":"111","pid":None,"childrenList":[]},
+    {"nid":2,"content":"222","pid":None,"childrenList":[]},
+    {"nid":3,"content":"333","pid":None,"childrenList":[]},
+    {"nid":4,"content":"444","pid":1,"childrenList":[]},
+    {"nid":5,"content":"555","pid":4,"childrenList":[]},
+    {"nid":6,"content":"666","pid":1,"childrenList":[]},
+    {"nid":7,"content":"777","pid":2,"childrenList":[]},
+]
+
+for comment_dict in commentList:
+    comment_dict["childrenList"]=[]
+
+    if comment_dict["pid"] == comment_dict["nid"]:
+
+        print(comment_dict["nid"])
+        print(comment_dict["pid"])

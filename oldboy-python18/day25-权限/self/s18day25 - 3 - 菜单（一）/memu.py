8c5add967b4ff7c04b91499e329d809fb942050b
huboa <wxcr11@gmail.com>
2018-01-06 17:20:01 +0800

    mac new

--- oldboy-python18/day25-权限/self/s18day25 - 3 - 菜单（一）/memu.py
+++ oldboy-python18/day25-权限/self/s18day25 - 3 - 菜单（一）/memu.py
@@ -1,12 +1,12 @@
 permission_menu_list = [
     {'id': 1, 'title': '用户列表', 'url': '/users/', 'pid': None, 'menu_id': 1, 'menu__name': '菜单1'},
-    # {'id': 2, 'title': '添加用户', 'url': '/users/add/', 'pid': 1, 'menu_id': 1, 'menu__name': '菜单1'},
-    # {'id': 3, 'title': '删除用户', 'url': '/users/del/(\d+)/', 'pid': 1, 'menu_id': 1,'menu__name': '菜单1'},
-    # {'id': 4, 'title': '修改用户', 'url': '/users/edit/(\d+)/', 'pid': 1, 'menu_id': 1,'menu__name': '菜单1'},
+    {'id': 2, 'title': '添加用户', 'url': '/users/add/', 'pid': 1, 'menu_id': 1, 'menu__name': '菜单1'},
+    {'id': 3, 'title': '删除用户', 'url': '/users/del/(\d+)/', 'pid': 1, 'menu_id': 1,'menu__name': '菜单1'},
+    {'id': 4, 'title': '修改用户', 'url': '/users/edit/(\d+)/', 'pid': 1, 'menu_id': 1,'menu__name': '菜单1'},
     {'id': 5, 'title': '主机列表', 'url': '/hosts/', 'pid': None, 'menu_id': 1, 'menu__name': '菜单1'},
-    # {'id': 6, 'title': '添加主机', 'url': '/hosts/add/', 'pid': 5, 'menu_id': 1, 'menu__name': '菜单1'},
-    # {'id': 7, 'title': '删除主机', 'url': '/hosts/del/(\d+)/', 'pid': 5, 'menu_id': 1,'menu__name': '菜单1'},
-    # {'id': 8, 'title': '修改主机', 'url': '/hosts/edit/(\d+)/', 'pid': 5, 'menu_id': 1,'menu__name': '菜单1'}
+    {'id': 6, 'title': '添加主机', 'url': '/hosts/add/', 'pid': 5, 'menu_id': 1, 'menu__name': '菜单1'},
+    {'id': 7, 'title': '删除主机', 'url': '/hosts/del/(\d+)/', 'pid': 5, 'menu_id': 1,'menu__name': '菜单1'},
+    {'id': 8, 'title': '修改主机', 'url': '/hosts/edit/(\d+)/', 'pid': 5, 'menu_id': 1,'menu__name': '菜单1'}
 ]
 # current_url = "/users/"
 current_url = "/users/del/1/"
@@ -26,7 +26,9 @@ for menu in permission_menu_list:
     if menu["pid"] == None:
         menu["active"]=True
         menu_list.append(menu)
-
+    else:
+        menuid=menu["pid"]
+        print(menu[menuid])
 print(menu_list)
 
 

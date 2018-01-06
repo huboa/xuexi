4b9e33b06e79ee7eaff4896f43a4bb762bafd8f0
huboa <wxcr11@gmail.com>
2018-01-06 17:45:02 +0800

    mac new

--- oldboy-python18/day25-权限/self/s18day25 - 3 - 菜单（一）/x3.py
+++ oldboy-python18/day25-权限/self/s18day25 - 3 - 菜单（一）/x3.py
@@ -1,4 +1,27 @@
-v1=[11,22]
-v2=v1
-v2.append(33)
-print(v1,v2)
+
+menu_list={
+    1: {'id': 1, 'title': '用户列表', 'url': '/users/', 'pid': None, 'menu_id': 1, 'menu__name': '菜单1', 'active': True},
+    5: {'id': 5, 'title': '主机列表', 'url': '/hosts/', 'pid': None, 'menu_id': 1, 'menu__name': '菜单1'},
+    10: {'id': 10, 'title': 'xx列表', 'url': '/hosts/', 'pid': None, 'menu_id': 2, 'menu__name': '菜单2'}
+}
+
+{
+    1: {
+        'menu__name': '菜单1',
+        'active': True,
+        'children': [
+            {'id': 1, 'title': '用户列表', 'url': '/users/', 'active': True}
+            {'id': 5, 'title': '主机列表', 'url': '/users/'}
+        ]
+    },
+    2: {
+        'menu__name': '菜单1',
+        'children': [
+            {'id': 10, 'title': 'xx列表', 'url': '/hosts/'}
+        ]
+
+    }
+}
+
+menu_list=[]
+print(menu_list)

c91b512d9087070ac7e98e36393ac3bc1b1b8ca2
huboa <wxcr11@gmail.com>
2018-01-06 17:50:01 +0800

    mac new

--- oldboy-python18/day25-权限/self/s18day25 - 3 - 菜单（一）/x3.py
+++ oldboy-python18/day25-权限/self/s18day25 - 3 - 菜单（一）/x3.py
@@ -2,15 +2,14 @@
 menu_list={
     1: {'id': 1, 'title': '用户列表', 'url': '/users/', 'pid': None, 'menu_id': 1, 'menu__name': '菜单1', 'active': True},
     5: {'id': 5, 'title': '主机列表', 'url': '/hosts/', 'pid': None, 'menu_id': 1, 'menu__name': '菜单1'},
-    10: {'id': 10, 'title': 'xx列表', 'url': '/hosts/', 'pid': None, 'menu_id': 2, 'menu__name': '菜单2'}
-}
+    10: {'id': 10, 'title': 'xx列表', 'url': '/hosts/', 'pid': None, 'menu_id': 2, 'menu__name': '菜单2'}}
 
-{
+menu_list2={
     1: {
         'menu__name': '菜单1',
         'active': True,
         'children': [
-            {'id': 1, 'title': '用户列表', 'url': '/users/', 'active': True}
+            {'id': 1, 'title': '用户列表', 'url': '/users/', 'active': True},
             {'id': 5, 'title': '主机列表', 'url': '/users/'}
         ]
     },
@@ -23,5 +22,6 @@ menu_list={
     }
 }
 
-menu_list=[]
-print(menu_list)
+
+for n in menu_list.values():
+    print(menu_list[n])

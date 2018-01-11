44e536ae4e0a93b4fee644a3a843542421b1bc81
huboa <wxcr11@gmail.com>
2018-01-11 23:50:02 +0800

    mac new

--- oldboy-python18/day25-权限/self/s18day25 - 1 - 用户登录获取所有权限/rbac/middleware/rbac.py
+++ oldboy-python18/day25-权限/self/s18day25 - 1 - 用户登录获取所有权限/rbac/middleware/rbac.py
@@ -20,28 +20,29 @@ class MiddlewareMixin(object):
 class RbacMiddleware(MiddlewareMixin):
     def proceess_request(self,request):
         #当前访问的URL
-        current_url = request.path_info
-        print(current_url,"rbac.py")
-        if current_url == '/login/':
-            return HttpResponse('当前用户无权限信息')
-        #当前用户的所有权限
-        permision_dic = request.seesion.get(settings.PERMISSIONS_DICT_SESSION_KEY)
-        # print(permision_dic)
-        # print(current_url)
-        tag=False
-        for item in permision_dic:
-            urls = item["urls"]
-            codes = item["codes"]
-            print(urls)
-            for rex in urls:
-                if re.match(rex,current_url):
-                    tag = True
-                    request.permission_codes = codes
-                    break
-            if tag:
-                break
-
-
-        if not tag:
-            return  HttpResponse('当前用户无权限信息')
+        print(request.path_info)
+        # current_url = request.path_info
+        # print(current_url,"rbac.py")
+        # if current_url == '/login/':
+        #     return HttpResponse('当前用户无权限信息')
+        # #当前用户的所有权限
+        # permision_dic = request.seesion.get(settings.PERMISSIONS_DICT_SESSION_KEY)
+        # # print(permision_dic)
+        # # print(current_url)
+        # tag=False
+        # for item in permision_dic:
+        #     urls = item["urls"]
+        #     codes = item["codes"]
+        #     print(urls)
+        #     for rex in urls:
+        #         if re.match(rex,current_url):
+        #             tag = True
+        #             request.permission_codes = codes
+        #             break
+        #     if tag:
+        #         break
+        #
+        #
+        # if not tag:
+        #     return  HttpResponse('当前用户无权限信息')
 

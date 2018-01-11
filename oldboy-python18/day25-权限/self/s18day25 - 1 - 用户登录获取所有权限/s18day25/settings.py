52c8d10f1fa722da0a495996933bee28c0ca9067
huboa <wxcr11@gmail.com>
2018-01-11 22:20:03 +0800

    mac new

--- oldboy-python18/day25-权限/self/s18day25 - 1 - 用户登录获取所有权限/s18day25/settings.py
+++ oldboy-python18/day25-权限/self/s18day25 - 1 - 用户登录获取所有权限/s18day25/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from rbac.middleware import rbac
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'rbac.middleware.rabc.RbacMiddleware',
+    'rabc.RbacMiddleware',
     # 'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 

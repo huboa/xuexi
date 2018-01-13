722159a2af703c1088926edb606e784f741c8004
huboa <wxcr11@gmail.com>
2018-01-13 15:25:02 +0800

    mac new

--- oldboy-python18/day26-curd-admin/self/curd/curd/urls.py
+++ oldboy-python18/day26-curd-admin/self/curd/curd/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from app01 import views
+from stark.service import v1
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^stark/', v1.site.urls),
+    
 ]

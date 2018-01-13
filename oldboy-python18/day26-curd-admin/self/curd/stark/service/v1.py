483b710130fb5d6c8cf19addab75b661dc45aa6b
huboa <wxcr11@gmail.com>
2018-01-13 15:45:02 +0800

    mac new

--- oldboy-python18/day26-curd-admin/self/curd/stark/service/v1.py
+++ oldboy-python18/day26-curd-admin/self/curd/stark/service/v1.py
@@ -1,23 +1,35 @@
-def register(self,model_class):
-    self.registry[model_class] = StarkConfig(model_class)
+from  django.shortcuts import HttpResponse
 
-@property
-def url(self):
-    from django.conf.urls import url
-    pts = [
-        url(r'^login/',self.login),
-    ]
+class StarkConfig(object):
+    def __init__(self,mcls):
+        self.mcls = mcls
 
-    for model_class,config_obj in self._registry.items():
-        app_label = model_class._meta.app_label
-        model_name = model_class._meta.model_name
-        temp = url(r'^%s/%s/' %(app_label,model_name),self.login)
-        pts.append(temp)
 
-    return pts,None,None
+class StarkSite(object):
+    def __init__(self):
+        self._registry ={}
 
-def login(self,request):
-    return HttpResponse('登录页面')
+
+    def register(self,model_class):
+        self.registry[model_class] = StarkConfig(model_class)
+
+    @property
+    def urls(self):
+        from django.conf.urls import url
+        pts = [
+            url(r'^login/',self.login),
+        ]
+
+        for model_class,config_obj in self._registry.items():
+            app_label = model_class._meta.app_label
+            model_name = model_class._meta.model_name
+            temp = url(r'^%s/%s/' %(app_label,model_name),self.login)
+            pts.append(temp)
+
+        return pts,None,None
+
+    def login(self,request):
+        return HttpResponse('登录页面')
 
 
 site = StarkSite()

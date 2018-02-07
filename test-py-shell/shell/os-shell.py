fd4b7df13dd750f2744760171a15a8ea43b0dae5
huboa <wxcr11@gmail.com>
2018-02-07 22:55:02 +0800

    mac new

--- test-py-shell/shell/os-shell.py
+++ test-py-shell/shell/os-shell.py
@@ -8,7 +8,7 @@ subprocess.call(["ls","-l"])
 subprocess.call(["ssh","root@192.168.56.202","ip","a"],)
 print("================")
 subprocess.call(["ssh","root@192.168.56.202","ip","a"],)
-out_bytes = subprocess.check_output(['ip','a'])
+out_bytes = subprocess.check_output(["ssh","root@192.168.56.202",'ip','a'])
 print(out_bytes)
 
 # (status,output)=subprocess.getstatusoutput(["ssh","root@192.168.56.202","ip","a"],)

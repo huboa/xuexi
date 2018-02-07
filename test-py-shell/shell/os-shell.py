f8ef364484b509802beff098747b611551ece4af
huboa <wxcr11@gmail.com>
2018-02-07 22:25:02 +0800

    mac new

--- test-py-shell/shell/os-shell.py
+++ test-py-shell/shell/os-shell.py
@@ -2,4 +2,4 @@ import subprocess
 subprocess.call(["ls","-l"])
 # subprocess.call(["ifconfig"])
 subprocess.call(["ifconfig","en0"])
-subprocess.call(["ssh","root@192.168.56.152"])
+subprocess.call(["ssh","root@192.168.56.202"])

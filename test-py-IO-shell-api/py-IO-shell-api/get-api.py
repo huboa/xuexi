a06f5cdb36c8bde4bae0bd67a95c51d86b15cd7f
huboa <wxcr11@gmail.com>
2018-02-07 23:35:02 +0800

    mac new

--- test-py-IO-shell-api/py-IO-shell-api/get-api.py
+++ test-py-IO-shell-api/py-IO-shell-api/get-api.py
@@ -1,2 +1,8 @@
 import requests
-url = ""
+import json
+url = "https://api.github.com/search/repositories?q=language:python&sort=starts"
+r=requests.get(url)
+print("Status code:",r.status_code,)
+response_dic =r.json()
+
+print(response_dic.keys())

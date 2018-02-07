06735a756d5ea91747e092dfbd4f85370910a2d4
huboa <wxcr11@gmail.com>
2018-02-07 23:00:01 +0800

    mac new

--- test-py-shell/shell/os-shell.py
+++ test-py-shell/shell/os-shell.py
@@ -1,5 +1,5 @@
 import subprocess
-
+import pexpect
 # subprocess.call(["ifconfig"])
 subprocess.call(["ifconfig","en0"])
 subprocess.call(["ls","-l"])
@@ -11,5 +11,7 @@ subprocess.call(["ssh","root@192.168.56.202","ip","a"],)
 out_bytes = subprocess.check_output(["ssh","root@192.168.56.202",'ip','a'])
 print(out_bytes)
 
-# (status,output)=subprocess.getstatusoutput(["ssh","root@192.168.56.202","ip","a"],)
-# print(status,output)
+
+
+
+pexpect.spawn
